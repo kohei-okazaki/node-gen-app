1b09660d2520161f61c81954919d615783158c48
kohei.okazaki <kou1210hei@gmail.com>
2020-01-03 17:52:15 +0900

    no message

--- node-test-app/routes/ontime/insert.js
+++ node-test-app/routes/ontime/insert.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mysql = require("mysql");
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 const mysql_setting = {
   host: 'localhost',
@@ -19,7 +20,15 @@ router.get("/", function (request, response, next) {
 
   var data = {
     title: "定時マスタ新規作成",
-    hasRecord: false
+    hasRecord: false,
+    form: {
+      companyCode: "",
+      workStartHour: "",
+      workStartMinute: "",
+      workEndHour: "",
+      workEndMinute: ""
+    },
+    errorMessageArray: []
   }
   response.render('ontime/insert', data);
 });
@@ -27,45 +36,113 @@ router.get("/", function (request, response, next) {
 /**
  * 新規レコード作成処理
  */
-router.post("/", function(request, response, next) {
+router.post("/", [
+  check("companyCode")
+    .notEmpty().withMessage("企業コードは必須です")
+    .isNumeric().withMessage("企業コードは数字です")
+    .isLength({ 
+      min: 5, 
+      max: 5
+    }).withMessage("企業コードは5桁です"),
 
-  var companyCode = request.body.companyCode;
-  var workStartHour = request.body.workStartHour;
-  var workStartMinute = request.body.workStartMinute;
-  var workEndHour = request.body.workEndHour;
-  var workEndMinute = request.body.workEndMinute;
-  var entity = {
-    "COMPANY_CODE": companyCode,
-    "WORK_START_HOUR": workStartHour,
-    "WORK_START_MINUTE": workStartMinute,
-    "WORK_END_HOUR": workEndHour,
-    "WORK_END_MINUTE": workEndMinute,
+  check("workStartHour")
+    .notEmpty().withMessage("始業時間(時)は必須です")
+    .isNumeric().withMessage("始業時間(時)は数字です")
+    .isLength({ 
+      min: 2, 
+      max: 2
+    }).withMessage("始業時間(時)は2桁です"),
+
+  check("workStartMinute")
+    .notEmpty().withMessage("始業時間(分)は必須です")
+    .isNumeric().withMessage("始業時間(分)は数字です")
+    .isLength({ 
+      min: 2, 
+      max: 2
+    }).withMessage("始業時間(分)は2桁です"),
+
+  check("workEndHour")
+    .notEmpty().withMessage("終業時間(時)は必須です")
+    .isNumeric().withMessage("終業時間(時)は数字です")
+    .isLength({ 
+      min: 2, 
+      max: 2
+    }).withMessage("終業時間(時)は2桁です"),
+
+  check("workEndMinute")
+    .notEmpty().withMessage("終業時間(分)は必須です")
+    .isNumeric().withMessage("終業時間(分)は数字です")
+    .isLength({ 
+      min: 2, 
+      max: 2
+    }).withMessage("終業時間(分)は2桁です")
+
+], function(request, response, next) {
+
+  let companyCode = request.body.companyCode;
+  let workStartHour = request.body.workStartHour;
+  let workStartMinute = request.body.workStartMinute;
+  let workEndHour = request.body.workEndHour;
+  let workEndMinute = request.body.workEndMinute;
+  let form = {
+    companyCode: companyCode,
+    workStartHour: workStartHour,
+    workStartMinute: workStartMinute,
+    workEndHour: workEndHour,
+    workEndMinute: workEndMinute
   };
 
+  // バリデーションの結果にエラーがあるかのチェック
+  const errors = validationResult(request);
+  if (errors.isEmpty()) {
+    // 妥当性チェックエラーでない場合
 
-  var connection = mysql.createConnection(mysql_setting);
-  connection.connect();
+    // Entityを作成
+    let entity = {
+      "COMPANY_CODE": companyCode,
+      "WORK_START_HOUR": workStartHour,
+      "WORK_START_MINUTE": workStartMinute,
+      "WORK_END_HOUR": workEndHour,
+      "WORK_END_MINUTE": workEndMinute,
+    };
 
-  // データを取り出す
-  let sql = "insert into ONTIME_MT set ?";
-  connection.query(sql, entity, function(error, results, fields) {
+    let connection = mysql.createConnection(mysql_setting);
+    connection.connect();
 
-    // データベースアクセス完了時の処理
-    if (error == null) {
-      // 正常終了の場合
-      var data = {
-        title: '定時マスタ登録結果',
-        entity: entity,
-        hasRecord: true
-      };
-      console.log('--> レンダリング');
-      response.render('ontime/insert', data);
-      console.log('<-- レンダリング');
-    } else {
-      console.log(error.stack);
-      response.render('ontime/insert', data);
-    }
-  });
+    // データを取り出す
+    let sql = "insert into ONTIME_MT set ?";
+    connection.query(sql, entity, function(error, results, fields) {
+
+      // データベースアクセス完了時の処理
+      if (error == null) {
+        // 正常終了の場合
+        let data = {
+          title: '定時マスタ登録結果',
+          entity: entity,
+          hasRecord: true,
+          form: form,
+          errorMessageArray: []
+        };
+        response.render('ontime/insert', data);
+      } else {
+        console.log(error.stack);
+        response.render('ontime/insert', data);
+      }
+    });
+
+  } else {
+    // 妥当性チェックエラーの場合
+
+    console.log(errors.array());
+    let data = {
+      title: '定時マスタ新規作成',
+      hasRecord: false,
+      form: form,
+      errorMessageArray: errors.array()
+    };
+    response.render('ontime/insert', data);
+
+  }
 
   connection.end();
 });
--- node-test-app/routes/ontime/select.js
+++ node-test-app/routes/ontime/select.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mysql = require("mysql");
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 const mysql_setting = {
   host: 'localhost',
@@ -18,50 +19,78 @@ router.get("/", function (request, response, next) {
 
   let data = {
     title: "定時マスタ検索",
-    hasRecord: false
+    hasRecord: false,
+    form: {
+      companyCode: ""
+    },
+    errorMessageArray: []
   }
   response.render('ontime/select', data);
 });
 
 /**
  * 検索処理
- * companyCodeが未指定の場合、全件検索を行う
+ * 企業コードが未指定の場合、全件検索を行う
  */
-router.post("/", function (request, response, next) {
+router.post("/", [
+
+  check("companyCode")
+    .isNumeric().withMessage("企業コードは数字です")
+
+], function (request, response, next) {
 
   console.log('定時マスタ検索画面を表示' + request.url);
 
-  var connection = mysql.createConnection(mysql_setting);
-  connection.connect();
-  let sql = 'SELECT * FROM ONTIME_MT';
-
-  // リクエストパラメータから検索SQLを作成
   let companyCode = request.body["companyCode"];
-  if (companyCode != null && companyCode != "") {
-    // 企業コードを指定されていた場合、検索条件に含める。
-    sql += " WHERE COMPANY_CODE = ?";
+  let form = {
+    companyCode: companyCode
   }
 
-  // データを取り出す
-  connection.query(sql, companyCode, function(error, results, fields) {
+  // バリデーションの結果にエラーがあるかのチェック
+  const errors = validationResult(request);
+  if (errors.isEmpty() || (companyCode === "")) {
+    // 妥当性チェックエラーでない場合
 
-    // データベースアクセス完了時の処理
-    if (error == null) {
-      let data = {
-        title: '定時マスタ検索',
-        content: results,
-        hasRecord: true
-      };
-
-      console.log('--> レンダリング');
-      // ※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※
-      // ※※※ 下の"ontime/select"の先頭に/を入れると404で落ちる  ※※※
-      // ※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※※
-      response.render('ontime/select', data);
-    } else {
-      console.log(error.stack);
+    var connection = mysql.createConnection(mysql_setting);
+    connection.connect();
+    let sql = 'SELECT * FROM ONTIME_MT';
+    if (companyCode != null && companyCode != "") {
+      // 企業コードを指定されていた場合、検索条件に含める。
+      sql += " WHERE COMPANY_CODE = ?";
     }
-  });
+
+    // データを取り出す
+    connection.query(sql, companyCode, function(error, results, fields) {
+
+      // データベースアクセス完了時の処理
+      if (error == null) {
+        let data = {
+          title: '定時マスタ検索',
+          content: results,
+          hasRecord: true,
+          form: form,
+          errorMessageArray: []
+        };
+
+        // ※※※ 下の"ontime/select"の先頭に/を入れると404で落ちる  ※※※
+        response.render('ontime/select', data);
+      } else {
+        console.log(error.stack);
+      }
+    });
+
+  } else {
+
+    let data = {
+      title: '定時マスタ検索',
+      hasRecord: false,
+      form: form,
+      errorMessageArray: errors.array()
+    };
+    response.render('ontime/select', data);
+
+  }
+
 
   connection.end();
 });
