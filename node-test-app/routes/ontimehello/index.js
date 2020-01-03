15f5f89eab0953c4286b9a661798cb5855aad547
kohei.okazaki <kou1210hei@gmail.com>
2020-01-03 23:20:52 +0900

    no message

--- node-test-app/routes/ontimehello/index.js
+++ node-test-app/routes/ontimehello/index.js
@@ -1,20 +1,39 @@
 var express = require("express");
 var mysql = require("mysql");
 var router = express.Router();
-const { check, validationResult } = require('express-validator');
 
-const mysql_setting = {
-  host: 'localhost',
-  port: '3306',
-  user: 'root',
-  password: 'admin',
-  database: 'kinwork'
-};
+const knex = require("knex")({
+  dialect: "mysql",
+  connection: {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'admin',
+    database: 'kinwork'
+  }
+});
+var Bookshelf = require("bookshelf")(knex);
+
+var ontimeMt = Bookshelf.Model.extend({
+  tableName: "ONTIME_MT"
+});
 
 router.get('/index', function(request, response, next) {
-
-
-  response.render('ontimehello/index', { title: 'Express' });
+  new ontimeMt().fetchAll().then((connection) => {
+    let data = {
+      title: "定時マスタ(BookShelf利用)",
+      content: connection.toArray()
+    }
+    response.render('ontimehello/index', data);
+  })
+  .catch(function(err) {
+    response.status(500).json({
+      error: true,
+      data: {
+        message: err.message
+      }
+    });
+  });
 });
 
 module.exports = router;
