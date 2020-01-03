4a1930185a035abd8d3ee0dc99a9e2f8f3c93833
kohei.okazaki <kou1210hei@gmail.com>
2020-01-04 00:32:30 +0900

    no message

--- node-test-app/routes/ontimehello/index.js
+++ node-test-app/routes/ontimehello/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var mysql = require("mysql");
 var router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 const knex = require("knex")({
   dialect: "mysql",
@@ -14,28 +15,87 @@ const knex = require("knex")({
 });
 var Bookshelf = require("bookshelf")(knex);
 
+// 定時マスタ
 var ontimeMt = Bookshelf.Model.extend({
   tableName: "ONTIME_MT"
 });
 
-router.get('/index', function(request, response, next) {
-  new ontimeMt().fetchAll().then((connection) => {
-    let data = {
-      title: "定時マスタ(BookShelf利用)",
-      content: connection.toArray()
+/**
+ * 定時マスタ検索のGET
+ * /select
+ */
+router.get('/select', function(request, response, next) {
+  console.log('定時マスタ検索画面を表示' + request.url);
+
+  let data = {
+    title: "定時マスタ検索(BookShelf利用)",
+    hasRecord: false,
+    form: {
+      companyCode: ""
+    },
+    errorMessageArray: []
+  };
+  response.render('ontimehello/select', data);
+
+  }
+);
+
+/**
+ * 定時マスタ検索のPOST
+ * 企業コードが未指定の場合、全件検索を行う
+ * /select
+ */
+router.post("/select", [
+
+  check("companyCode")
+    .isNumeric().withMessage("企業コードは数字です")
+
+], function(request, response, next) {
+
+  console.log('定時マスタ検索画面を表示' + request.url);
+
+  let companyCode = request.body["companyCode"];
+  let form = {
+    companyCode: companyCode
+  };
+
+  // バリデーションの結果にエラーがあるかのチェック
+  const errors = validationResult(request);
+
+  if (errors.isEmpty() || (companyCode === "")) {
+    // 妥当性チェックエラーでない場合
+    if (companyCode === "") {
+      // 企業コードが未指定の場合、全件検索
+      new ontimeMt().fetchAll().then((connection) => {
+        let data = {
+          title: "定時マスタ検索(BookShelf利用)",
+          content: connection.toArray(),
+          errorMessageArray: [],
+          hasRecord: true,
+          form: form
+        };
+        response.render('ontimehello/select', data);
+      })
+      .catch(function(err) {
+        response.status(500).json({
+          error: true,
+          data: {
+            message: err.message
+          }
+        });
+      });
     }
-    response.render('ontimehello/index', data);
-  })
-  .catch(function(err) {
-    response.status(500).json({
-      error: true,
-      data: {
-        message: err.message
-      }
-    });
-  });
+
+  } else {
+    // 指定した企業コードで検索
+  }
+
 });
 
+/**
+ * 定時マスタ登録のGET
+ * /insert
+ */
 router.get("/insert", function(request, response, next) {
   console.log('定時マスタ登録画面を表示' + request.url);
   var data = {
@@ -53,7 +113,53 @@ router.get("/insert", function(request, response, next) {
   response.render('ontimehello/insert', data);
 });
 
-router.post("/insert", function(request, response, next) {
+/**
+ * 定時マスタ登録のPOST
+ * /insert
+ */
+router.post("/insert", [
+
+  check("companyCode")
+    .notEmpty().withMessage("企業コードは必須です")
+    .isNumeric().withMessage("企業コードは数字です")
+    .isLength({ 
+      min: 5, 
+      max: 5
+    }).withMessage("企業コードは5桁です"),
+
+  check("workStartHour")
+    .notEmpty().withMessage("始業時間(時)は必須です")
+    .isNumeric().withMessage("始業時間(時)は数字です")
+    .isLength({ 
+      min: 2, 
+      max: 2
+    }).withMessage("始業時間(時)は2桁です"),
+
+  check("workStartMinute")
+    .notEmpty().withMessage("始業時間(分)は必須です")
+    .isNumeric().withMessage("始業時間(分)は数字です")
+    .isLength({ 
+      min: 2, 
+      max: 2
+    }).withMessage("始業時間(分)は2桁です"),
+
+  check("workEndHour")
+    .notEmpty().withMessage("終業時間(時)は必須です")
+    .isNumeric().withMessage("終業時間(時)は数字です")
+    .isLength({ 
+      min: 2, 
+      max: 2
+    }).withMessage("終業時間(時)は2桁です"),
+
+  check("workEndMinute")
+    .notEmpty().withMessage("終業時間(分)は必須です")
+    .isNumeric().withMessage("終業時間(分)は数字です")
+    .isLength({ 
+      min: 2, 
+      max: 2
+    }).withMessage("終業時間(分)は2桁です")
+
+], function(request, response, next) {
   let res = response;
   let entity = {
     "COMPANY_CODE": request.body.companyCode,
@@ -62,12 +168,14 @@ router.post("/insert", function(request, response, next) {
     "WORK_END_HOUR": request.body.workEndHour,
     "WORK_END_MINUTE": request.body.workEndMinute,
   };
+
+  // DB登録
   new ontimeMt(entity).save().then(function(model) {
     let data = {
       title: "定時マスタ登録(BookShelf利用)",
       entity: entity
     }
-    response.render('ontimehello/index', data);
+    response.render('ontimehello/insert', data);
   });
 });
 
